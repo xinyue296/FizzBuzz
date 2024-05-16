a7da4d361581ed7d64ca6a43753725348c359ac7
Cindy Tao <cindytao.xy@outlook.com>
2024-05-16 19:24:36 -0400

    learn about prive and public method; finish with Task 3

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // psvm: short path generating `public static void main()`
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        // this following line wont work since there are no argument inside the brackets;
+        // FizzBuzz.main();
+    }
+}
